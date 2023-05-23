388b23b3293400babf23f50b932913d8c88ad1a2
Owais Abbas <83211298+owaisabbas786@users.noreply.github.com>
2023-05-24 01:35:41 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,27 @@
+
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({path:'./.env.local'});
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY
+
+module.exports = {
+  solidity: "0.8.10",
+  defaultNetwork: "polygon",
+  networks: {
+    hardhat: {},
+    polygon: {
+      url: process.env.NEXT_PUBLIC_RPC_URL,
+      accounts: [privateKey]
+    }
+  }
+};
+
+
